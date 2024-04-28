1d5287baa00e01a6fea52c45f9cd3755d4b7140f
James Chen <jameschen0125@pu.edu.tw>
2024-04-28 23:02:04 +0800

    加入專案檔案。

---
+++ Form1.cs
@@ -0,0 +1,15 @@
+using System.Windows.Forms;
+
+namespace TodoListApp
+{
+    public partial class Form1 : Form
+    {
+        //private TodoList todoList;
+        //private string filePath = "todolist.json";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
